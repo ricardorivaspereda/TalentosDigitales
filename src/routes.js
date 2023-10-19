8d6a067dd988aa86632f0780c4e7164483f0a7d5
ricardorivaspereda <97137363+ricardorivaspereda@users.noreply.github.com>
2023-10-18 21:37:07 -0300

    Delete src directory

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { libro } from "./controller.js";
-
-export const router = Router()
-
-router.get('/libros', libro.getAll);
-router.post('/libro', libro.add);
-router.delete('/libro', libro.delete);
-router.put('/libro', libro.update);
